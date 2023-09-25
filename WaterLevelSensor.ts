4cc8b4c43de70ffd855953f16df06747fe418bbc
lukas883 <72947801+lukas883@users.noreply.github.com>
2023-09-25 09:20:37 +0200

    Aktualisiere pxt.json, main.blocks, main.ts, WaterLevelSensor.ts

--- WaterLevelSensor.ts
+++ WaterLevelSensor.ts
@@ -2,23 +2,30 @@
 
 //% color="#AA278D" weight=100
 namespace WaterLevelSensor {
+    /**
+     * returns the Water Level with a number between 0 and 100
+     */
     //% block
     export function getPercentage() {
         let count = 0
         let THREASHOLD = 245
         for (let index = 0; index < 8; index++) {
+            serial.writeNumber(pins.i2cReadNumber(119, NumberFormat.UInt8LE, false))
+            serial.writeString(".")
             if (pins.i2cReadNumber(119, NumberFormat.UInt8LE, false) > THREASHOLD) {
                 count++
             }
         }
         for (let index = 0; index < 12; index++) {
+            serial.writeNumber(pins.i2cReadNumber(120, NumberFormat.UInt8LE, false))
+            serial.writeString(".")
             if (pins.i2cReadNumber(120, NumberFormat.UInt8LE, false) > THREASHOLD) {
                 count++
             }
         }       
         
         //serial.writeNumber(count)
-        //serial.writeLine("  ")
+        serial.writeLine("  ")
         return count * 5
     }
     /**
@@ -39,6 +46,11 @@ namespace WaterLevelSensor {
             led.unplot(i % 5, 4 - Math.floor(i / 5))
         }
     }
+    /**
+     * makes a sound if the Water Level drops below a certain threshold
+     * @param threshold Water level that triggers the LowLevelAlert
+     * param level is the return value of getPercentage()
+     */
     //%block
     export function LowLevelAlert(threshold:number, level:number){
         if(level<threshold){
@@ -46,6 +58,22 @@ namespace WaterLevelSensor {
         }
         else{
             music.stopAllSounds()
+        }
+    }
+    /**
+     * controls a pump through pin C16 so that the water level will stay between min and max
+     * @param min starts pump
+     * @param max stops pump
+     * @param level is the Water level from getPercentage()
+     */
+    //%block
+    export function controlPump(min:number, max:number, level:number){
+        if (level<min){
+            //turn on the Pump through Pin C16 (electronic Switch)
+            pins.digitalWritePin(DigitalPin.C16, 1)
+        } 
+        else if(level>max){
+            pins.digitalWritePin(DigitalPin.C16, 0)
         }
     }
 }// Gib deinen Code hier ein
--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
 basic.forever(function () {
     WaterLevelSensor.showOnDisplay(WaterLevelSensor.getPercentage())
     WaterLevelSensor.LowLevelAlert(20, WaterLevelSensor.getPercentage())
+    WaterLevelSensor.controlPump(20, 50, WaterLevelSensor.getPercentage())
 })
