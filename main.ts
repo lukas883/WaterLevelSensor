62ddec4cb93118a6e0cfa2c7b9c4a7050aa1a0c3
lukas883 <72947801+lukas883@users.noreply.github.com>
2023-11-28 07:12:12 +0100

    Update main.ts
    If this has the on forever function, you will not be able to use the blocks in your Program individually

--- main.ts
+++ main.ts
@@ -1,5 +1,1 @@
-basic.forever(function () {
-    WaterLevelSensor.showOnDisplay(WaterLevelSensor.getPercentage())
-    WaterLevelSensor.LowLevelAlert(20, WaterLevelSensor.getPercentage())
-    WaterLevelSensor.controlPump(20, 50, WaterLevelSensor.getPercentage())
-})
+
